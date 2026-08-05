2a0acec1153b64b59ce00d96e13155d730a58f2b
Chokri Samaali <chokri.samali@gmail.com>
2026-08-06 00:24:52 +0100

    published to the web

--- get_exercises.php
+++ get_exercises.php
@@ -6,6 +6,7 @@ $dbname = 'solutions_python';
 $username = 'root';
 $password = '';
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
